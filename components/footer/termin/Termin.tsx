df58e9e1b46ca0ebb3807b679b79d902f1f5256c
Peter Hofmann <peter@studiohofmann.de>
2024-12-24 13:19:16 +0100

    Colors

--- components/footer/termin/Termin.tsx
+++ components/footer/termin/Termin.tsx
@@ -20,7 +20,7 @@ export default async function Termin() {
     if (!terminData) return null
 
     return (
-        <div className='footersection px-4 py-16 md:px-48 bg-red-500'>
+        <div className='footersection px-4 py-16 md:px-48 bg-grey-700'>
             <div className='flex flex-col md:flex-row gap-8 md:gap-16'>
                 <div className='md:flex-1 text-green-200'>
                     <PortableText value={terminData.text || []} />
